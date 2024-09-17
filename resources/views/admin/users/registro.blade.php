0960d079448e3f34eaa70f6bc8612c0d55b410c5
CristhianCanar <cristhian.canar.m@gmail.com>
2024-09-16 21:25:18 -0500

    Primer commit

---
+++ resources/views/admin/users/registro.blade.php
@@ -0,0 +1,10 @@
+@extends('admin.layouts.app')
+@section('content')
+    <div class="page-inner">
+        <div class="col-12">
+            <div class="card">
+                <h1>Nuevo elemento </h1>
+            </div>
+        </div>
+    </div>
+@endsection
